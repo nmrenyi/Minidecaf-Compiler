311816ce1ff594f5f0960f153cf66df8f7c4939e
任一 <2018011423@secoder.net>
2020-10-20 10:45:19 +0800

    refactor to AsmCommand.py

--- minidecaf/AsmCommand.py
+++ minidecaf/AsmCommand.py
@@ -1,10 +1,12 @@
 # These Command Structures are copied from TA's reference implementations.
 # They have very clear structure and good scalability. So I used them directly in my step1 as a good starting structure.
 
+
 class AsmCommand:
-    '''
+    """
     base class for asm commands
-    '''
+    """
+
     def __init__(self, s):
         self.s = s
 
@@ -13,23 +15,28 @@ class AsmCommand:
 
 
 class AsmLabel(AsmCommand):
-    '''
+    """
     represent labels like 'main'
-    '''
+    """
+
     def __str__(self):
         return f"{self.s}:"
 
+
 class AsmInstruction(AsmCommand):
-    '''
+    """
     represent instructions like 'jr ra'
-    '''
+    """
+
     def __str__(self):
         return f"\t{self.s}"
 
+
 class AsmInstructionList(AsmCommand):
-    '''
+    """
     concatenate instructions in the list
-    '''
+    """
+
     def __str__(self):
         # self.s is a list of str
         local_s = ''
@@ -37,10 +44,11 @@ class AsmInstructionList(AsmCommand):
             local_s += '\t' + string + '\n'
         return local_s
 
+
 class AsmDirective(AsmCommand):
-    '''
-    asm directives like '.globl', '.text'
-    '''
+    """
+    asm directives like '.global', '.text'
+    """
+
     def __str__(self):
         return f"\t{self.s}"
-
